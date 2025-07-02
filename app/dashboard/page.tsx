41a74ea022a7bba80100e01cffff3788e461a75a
EllisOllier <ellisollier1@gmail.com>
2025-07-02 21:59:59 +0100

    Added dashboard page and updated signin/up to redirect to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,14 @@
-export default function Dashboard() {
-  return (
-    <div className="">
-      <h1>Dashboard</h1>
-    </div>
-  );
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export default async function Dashboard() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+
+  return <p>Hello {data.user.email}</p>
 }
