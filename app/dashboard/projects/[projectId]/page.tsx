70a28bbcd42a4028b9b5d5174f7dc77dc7659bc3
EllisOllier <ellisollier1@gmail.com>
2025-09-30 13:28:43 +0100

    Integrated Lexical to project page. Needs theme to be finished.

--- app/dashboard/projects/[projectId]/page.tsx
+++ app/dashboard/projects/[projectId]/page.tsx
@@ -1,12 +1,17 @@
+import Editor from '@/component/Editor'
+
 export default async function Project({
-  params,
+    params,
 }: {
-  params: Promise<{ projectId: string }>
-}){
+    params: Promise<{ projectId: string }>;
+}) {
+    // Wait for projectId passed into function
     const { projectId } = await params;
+
     return (
         <div>
             <h1>Project: {projectId}</h1>
+            <Editor />
         </div>
-    )
+    );
 }
